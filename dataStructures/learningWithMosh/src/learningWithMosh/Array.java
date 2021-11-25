4b6d4f0829b323f301fa5a42f9a86caed4e851a2
Warunee Dinunzio <dinunziow@gmail.com>
2021-11-24 21:52:40 -0500

    data structures

---
+++ dataStructures/learningWithMosh/src/learningWithMosh/Array.java
@@ -0,0 +1,22 @@
+package learningWithMosh;
+
+public class Array {
+	private int[] items;
+	private int count;
+	public Array(int length) {
+		items = new int[length];
+	}
+	
+	public void insert(int item) {
+		// If the array is full, resize it
+		// Add the new item at the end
+		items[count++] = item;
+	}
+	
+	
+	public void print() {
+		for (int i = 0; i < count; i++)
+			System.out.println(items[i]);
+	}
+	
+}
---
+++ dataStructures/learningWithMosh/src/learningWithMosh/Main.java
@@ -0,0 +1,18 @@
+package learningWithMosh;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		Array numbers = new Array(length:3);
+//		int[] numbers = {10, 20, 30};
+//		System.out.println(numbers.length);
+//		System.out.println(Arrays.toString(numbers));
+		
+		//Arrays numbers = new Arrays();
+		
+
+	}
+
+}
